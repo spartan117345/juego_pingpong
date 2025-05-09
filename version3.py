9ed8dbf042b41c469eeac6c0738ba20b98fb9a2f
spartan117345 <juliansanchez2@colegioguanenta.edu.co>
2025-05-09 07:10:12 -0500

    se modificaron y crearon  nuevos archivos

--- version3.py
+++ version3.py
@@ -36,13 +36,15 @@ XX2 = 600
 YY2 = 280
 YY3 = 315
 XX3 = 410
+YY4 = -600
+XX4 = 0
 movimientoX = 3
 movimientoY = 1
 
 # varibales de puntos
 puntos_1 = 0
 puntos_2 = 0
-
+tiempo = 0
 # Bucle principal
 while True:
     clock.tick(50)  # Limita los FPS a 50
@@ -53,26 +55,32 @@ while True:
             pygame.quit()
             sys.exit()
 
+    pygame.draw.rect(ventana,blanco,(500,100,600,20))
+    pygame.draw.line(ventana, blanco, (410,150),(410,450),4)
     
     XX23 = XX2 + 20
     YY23 = YY2 + 60
 
     if XX3 >= XX2 and XX3 <= XX23 and YY3 >= YY2 and YY3 <= YY23:
-        movimientoX = -3
+        movimientoX = -5
 
     XX24 = XX1 + 20
     YY24 = YY1 + 60
 
     if XX3 >= XX1 and XX3 <= XX24 and YY3 >= YY1 and YY3 <= YY24:
-        movimientoX = 3
+        movimientoX = 5
     
 
     # Limpiar pantalla con color negro
     ventana.fill(negro)
 
-    # movimiento de la pelota
-    XX3 = XX3 + movimientoX   
-    YY3 =  movimientoY + YY3
+    # tiempo de inicio de juego
+    tiempo = tiempo + 1
+    if tiempo >= 900:
+        # movimiento de la pelota
+        XX3 = XX3 + movimientoX   
+        YY3 =  movimientoY + YY3
+
     # Dibujar elementos
     pygame.draw.rect(ventana, blanco, (800, 20, 0, 250))
     pygame.draw.line(ventana, blanco, (0, 150), (800, 150), 3)
@@ -130,9 +138,18 @@ while True:
 
     # texto de la version del juego
     fuente_arial = pygame.font.SysFont("Arial", 35, 1, 1)
-    texto = fuente_arial.render("version 1.2.0",3, gris_mas_oscuro)
+    texto = fuente_arial.render("version 1.3.0",3, gris_mas_oscuro)
     ventana.blit(texto,(580,450))
-    
+
+    # texto de los puntos del juego
+    fuente_arial = pygame.font.SysFont("Arial", 50, 1, 1)
+    texto = fuente_arial.render(str(puntos_1),3, negro)
+    ventana.blit(texto,(390,510))
+
+    fuente_arial = pygame.font.SysFont("Arial", 50, 1, 1)
+    texto = fuente_arial.render(str(puntos_2),3, negro)
+    ventana.blit(texto,(390,50))
+
     a = -1
     b = 1
 
@@ -160,12 +177,17 @@ while True:
     if XX2 <= 410:
         XX2 = XX2 + 5
 
+    if XX3 >= 790 or XX3 <= 0:
+            XX3 = 410
+            YY3 = 315
+            tiempo = 800
+    
+    
     if XX3 >= 790:
-        XX3 = 410
-
+        puntos_1 = puntos_1 + 1
 
     if XX3 <= 0:
-        XX3 = 410
+        puntos_2 = puntos_2 + 1
     
     if YY3 >= 450:
         movimientoY = -1
@@ -173,6 +195,11 @@ while True:
     if YY3 <= 150:
         movimientoY = 1
 
+    if puntos_1 >= 0:
+        # texto de los puntos del juego
+        fuente_arial = pygame.font.SysFont("Arial", 50, 1, 1)
+        texto = fuente_arial.render(str(puntos_1),True, negro)
+        ventana.blit(texto,(390,510))
     
     # Actualizar la pantalla
     pygame.display.flip()
